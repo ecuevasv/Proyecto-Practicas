e231b371f8564b67a0e371dc29e0506debd1a7fd
Edison Cuevas <ecuevas@wr.org>
2023-06-26 11:05:20 -0600

    Creando estructura del proyecto, archivos create-resource.ps1 y destroy-resource.ps1

---
+++ create-resources.ps1
@@ -0,0 +1,6 @@
+# Declarar variables
+$name = "myresourcegroup"
+$location = "eastus"
+
+# Creando un grupo de recursos
+az group create --name $name --location $location
---
+++ destroy-resources.ps1
@@ -0,0 +1,6 @@
+# Delcarar varaibles
+$name = "myresourcegroup"
+$location = "eastus"
+
+# Eliminando grupo de recursos
+az group delete --name $name
